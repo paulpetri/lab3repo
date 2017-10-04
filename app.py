43a6582c8c0fcabb5e3b89d1fb83f7cbdba643c4
Paul <c15735611@mydit.ie>
2017-10-04 16:12:00 +0000

    update working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_mysqldb import MySQL
 mysql = MySQL()
 app = Flask(__name__)
@@ -13,23 +14,33 @@ mysql.init_app(app)
 # The first route to access the webservice from http://external-ip:5000/ 
 #@pp.route("/add") this will create a new endpoints that can be accessed using http://external-ip:5000/add
 
+
+
+@app.route("/add_new_record", methods = ['GET'])
+def add_new():
+	name = request.args.get('name')
+	email = request.args.get('email')
+	rv=('''INSERT INTO students (studentName, email) values("{}","{}");'''.format(name, email) )
+#	return(rv)
+#	conn = mysql.connect()
+	cur = mysql.connection.cursor()
+	requeststring=request.url
+	cur.execute(rv)
+	mysql.connection.commit()
+	return('''success!''')
+
+@app.route("/update_info/")
+def update_info(): 
+	cur = mysql.connection.cursor()
+	cur.execute('''UPDATE student SET (studentName=%s, email=%s WHERE  studentName=%s)''',(newfirstName, email1@gmail.com, "first student"))
+	conn.commit()
+
 @app.route("/")
 def hello(): # Name of the method
     cur = mysql.connection.cursor() #create a connection to the SQL instance
     cur.execute('''SELECT * FROM students''') # execute an SQL statment
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
     return str(rv)      #Return the data in a string format
-
-@app.route("/add_new_record/")
-def add_new():
-	cur = mysql.connec
-	cur.execute('''INSERT INTO student (studentName, email) values("third student", "third@mail.com")''')
-
-@app.route("/update_info/")
-def update_info(): 
-	cur = mysql.connection.cursor()
-	cur.execute('''UPDATE student SET (studentName=%s, email=%s WHERE  studentName=%s)''',(newfirstName, email1@gmail.com, "first student"))
-
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port='12398') #Run the flask app at port 5000
